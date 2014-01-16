94df7cf63f4d969bda44845dba880871176467b8
Alexander Salas <a.salas@ieee.org>
2014-01-16 16:44:21 -0430

    Under heavy development

---
+++ domain.js
@@ -0,0 +1,41 @@
+/*global require, exports */
+
+(function () {
+    "use strict";
+
+    var child_process = require("child_process"),
+        domainName = "jekyll.execute";
+
+    function jekyll(directory, command, callback) {
+        child_process.exec(command, { cwd: directory}, function (err, stdout, stderr) {
+            callback(err ? stderr : undefined, err ? undefined : stdout);
+        });
+    }
+	
+    exports.init = function (DomainManager) {
+        if (!DomainManager.hasDomain(domainName)) {
+            DomainManager.registerDomain(domainName, {
+                major: 0,
+                minor: 1
+            });
+        }
+        
+        DomainManager.registerCommand(domainName, "jekyll", jekyll, true, "Exec Jekyll cmd",
+            [
+                {
+                    name: "directory",
+                    type: "string"
+                },
+                {
+                    name: "command",
+                    type: "string"
+                }
+            ],
+            [{
+                name: "stdout",
+                type: "string"
+            }]
+        );
+    };
+    	
+}());
--- main.js
+++ main.js
@@ -5,26 +5,178 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
-
-    
+    /** --- MODULES --- **/
+    var CommandManager  		= brackets.getModule("command/CommandManager"),
+		Commands                = brackets.getModule("command/Commands"),
+        Menus           		= brackets.getModule("command/Menus"),
+		ProjectManager        	= brackets.getModule("project/ProjectManager"),
+		ExtensionUtils  		= brackets.getModule("utils/ExtensionUtils"),
+		Dialogs					= brackets.getModule("widgets/Dialogs"),
+		NodeConnection  		= brackets.getModule("utils/NodeConnection"),
+		nodeConnection  		= new NodeConnection(),
+		domainPath				= ExtensionUtils.getModulePath(module) + "domain",
+		JekyllMenuID			= "jekyll-menu",
+		JekyllMenu				= Menus.addMenu("Jekyll", JekyllMenuID),
+		JEKYLL_IMPORT_DIALOG_ID	= "jekyll-import-dialog";
+		
+	var curProjectDir,
+		cmd = '';
     // Function to run when the menu item is clicked
     function handleJekyllServe() {
         window.alert("ToDo: bundle exec jekyll serve -t -w");
     }
+
+	function handleJekyllDoctor() {
+ 		curProjectDir = ProjectManager.getProjectRoot().fullPath;
+		
+		nodeConnection.connect(true).fail(function (err) {
+			console.error("[[Brackets Jekyll]] Cannot connect to node: ", err);
+		}).then(function () {			
+			return nodeConnection.loadDomains([domainPath], true).fail(function (err) {
+				console.error("[[Brackets Jekyll]] Cannot register domain: ", err);
+			});
+		}).then(function () {
+			nodeConnection.domains["jekyll.execute"].jekyll(curProjectDir, 'bundle exec jekyll doctor')
+			.fail(function (err) {
+				console.error("[[Brackets Jekyll]] fail: ", err);
+			})
+			.then(function (data) {
+				console.log("[[Brackets Jekyll]] then: " + data);
+				Dialogs.showModalDialog(Dialogs.DIALOG_ID_ERROR, "Jekyll Doctor", data);
+			});
+		}).done();
+	}
+	
+	function handleJekyllNewSite() {
+ 		ProjectManager.openProject().done(function () {
+			curProjectDir = ProjectManager.getProjectRoot().fullPath;
+			nodeConnection.connect(true).fail(function (err) {
+				console.error("[[Brackets Jekyll]] Cannot connect to node: ", err);
+			}).then(function () {			
+				return nodeConnection.loadDomains([domainPath], true).fail(function (err) {
+					console.error("[[Brackets Jekyll]] Cannot register domain: ", err);
+				});
+			}).then(function () {
+				nodeConnection.domains["jekyll.execute"].jekyll(curProjectDir, 'bundle exec jekyll new '+ curProjectDir)
+				.fail(function (err) {
+					console.error("[[Brackets Jekyll]] fail: ", err);
+				})
+				.then(function (data) {
+					console.log("[[Brackets Jekyll]] then: " + data);
+					Dialogs.showModalDialog(Dialogs.DIALOG_ID_ERROR, "Jekyll Site", data).done(function () {
+						ProjectManager.refreshFileTree();
+						//ProjectManager.openProject(curProjectDir);
+						//CommandManager.execute(Commands.APP_RELOAD);
+					});
+				});
+			}).done();
+		});		
+	}
+	
     
+    var Dialog = {
+		/**
+		 * The import modal is used to install a module inside the directory of the current file
+		 * HTML: html/modal-import.html
+		 */
+		importBlog: {
+			
+            /**
+             * HTML put inside the dialog
+             */
+            html: require("text!html/modal-import.html"),
+            
+			/**
+			 * Opens up the modal
+			 */
+			show: function() {
+				
+                Dialogs.showModalDialog(
+                    JEKYLL_IMPORT_DIALOG_ID, // ID the specify the dialog
+                    "Import your old & busted site or blog", // Title
+                    this.html,               // HTML-Content
+                    [                        // Buttons
+                        {className: Dialogs.DIALOG_BTN_CLASS_PRIMARY, id: Dialogs.DIALOG_BTN_OK, text: "Import"},
+                        {className: Dialogs.DIALOG_BTN_CLASS_NORMAL, id: Dialogs.DIALOG_BTN_CANCEL, text: "Cancel"}
+                    ]
+                ).done(function(id) {
+					
+					// Only saving
+					if(id !== "ok") return;
+					
+					// Module name musn't be empty
+					if(source.value == "") {
+						Dialogs.showModalDialog(Dialogs.DIALOG_ID_ERROR, "Error", "Please enter the XML file of your blog’s content.");
+						return;
+					}
+					console.log(source.value);
+					console.log(blog.value);
+					
+					nodeConnection.connect(true).fail(function (err) {
+						console.error("[[Brackets Jekyll]] Cannot connect to node: ", err);
+					}).then(function () {			
+						return nodeConnection.loadDomains([domainPath], true).fail(function (err) {
+							console.error("[[Brackets Jekyll]] Cannot register domain: ", err);
+						});
+					}).then(function () {
+						nodeConnection.domains["jekyll.execute"].jekyll(curProjectDir, 'bundle exec jekyll import '+ blog.value + ' --source ' + source.value)
+						.fail(function (err) {
+							console.error("[[Brackets Jekyll]] fail: ", err);
+						})
+						.then(function (data) {
+							console.log("[[Brackets Jekyll]] then: " + data);
+							Dialogs.showModalDialog(Dialogs.DIALOG_ID_ERROR, "Jekyll Import", data).done(function () {
+								ProjectManager.refreshFileTree();
+								//ProjectManager.openProject(curProjectDir);
+								//CommandManager.execute(Commands.APP_RELOAD);
+							});
+						});
+					}).done();
+					
+					// Should it be saved to package.json
+					//var s = save.checked ? "--save" : "";
+					
+					//ConnectionManager.new([name.value, s], "install");
+					
+				});
+				
+				// It's important to get the elements after the modal is rendered but before the done event
+				var source = document.querySelector("." + JEKYLL_IMPORT_DIALOG_ID + " .source"),
+					blog = document.querySelector("." + JEKYLL_IMPORT_DIALOG_ID + " .blog");
+			}
+        }
+    };
     
     // First, register a command - a UI-less object associating an id to a handler
-    var JEKYLL_SERVE_COMMAND_ID = "jekyll.serve";   // package-style naming to avoid collisions
-    CommandManager.register("Exec Jekyll", JEKYLL_SERVE_COMMAND_ID, handleJekyllServe);
-
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(JEKYLL_SERVE_COMMAND_ID);
-    
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(JEKYLL_SERVE_COMMAND_ID, "Ctrl-Alt-H");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
+    var JEKYLL_SERVE_CMD_ID = "jekyll.serve";   // package-style naming to avoid collisions
+    CommandManager.register("Run server", JEKYLL_SERVE_CMD_ID, handleJekyllServe);
+    var JEKYLL_DOCS_CMD_ID = "jekyll.docs";   // package-style naming to avoid collisions
+    CommandManager.register("Local documentation", JEKYLL_DOCS_CMD_ID, handleJekyllServe);
+    var JEKYLL_DOCTOR_CMD_ID = "jekyll.doctor";   // package-style naming to avoid collisions
+    CommandManager.register("Search deprecation warnings", JEKYLL_DOCTOR_CMD_ID, handleJekyllDoctor);
+    var JEKYLL_IMPORT_CMD_ID = "jekyll.import";   // package-style naming to avoid collisions
+    CommandManager.register("Import your old blog", JEKYLL_IMPORT_CMD_ID,  function() {
+		Dialog.importBlog.show();
+	});
+    var JEKYLL_NEW_CMD_ID = "jekyll.new";   // package-style naming to avoid collisions
+	CommandManager.register("New site scaffold", JEKYLL_NEW_CMD_ID, handleJekyllNewSite);
+	
+/*     
+	build                Build your site
+    default
+    docs                 Launch local server with docs for Jekyll v1.4.3
+    doctor               Search site and print specific deprecation warnings
+    help                 Display global or [command] help documentation.
+    import               Import your old blog to Jekyll
+    new                  Creates a new Jekyll site scaffold in PATH
+    serve                Serve your site locally */
+	
+    JekyllMenu.addMenuItem(JEKYLL_SERVE_CMD_ID, "Alt-J");
+	JekyllMenu.addMenuDivider();
+	JekyllMenu.addMenuItem(JEKYLL_NEW_CMD_ID);
+	JekyllMenu.addMenuDivider();
+	JekyllMenu.addMenuItem(JEKYLL_DOCTOR_CMD_ID);
+	JekyllMenu.addMenuItem(JEKYLL_IMPORT_CMD_ID);
+	JekyllMenu.addMenuDivider();
+	JekyllMenu.addMenuItem(JEKYLL_DOCS_CMD_ID);
 });
